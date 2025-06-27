dddcb70c3c2b00cd23420db2e47ddb9cd7d416fc
Laith Ghnemat <laithghnemat4@gmail.com>
2025-06-28 02:15:47 +0300

    Integrated Keycloak with OAuth2 and secured microservices

---
+++ API-Gateway/src/main/java/com/api/gateway/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.api.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static ch.qos.logback.classic.spi.ThrowableProxyVO.build;
+
+@Configuration
+public class SecurityConfig {
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.authorizeHttpRequests
+                        (authorization -> authorization.anyRequest()
+                                .authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .build();
+
+    }
+}
